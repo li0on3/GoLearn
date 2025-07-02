cbed390e93f5adc02ca00c0fab890ccf10147067
musyor <3072342792@qq.com>
2025-07-02 22:42:07 +0800

    feat: 完成day02切片深入学习练习

--- exercises/01-basics/day02/slice_algorithms.go
+++ exercises/01-basics/day02/slice_algorithms.go
@@ -1,7 +1,30 @@
 package main
 
+import (
+	"fmt"
+	"sort"
+)
+
+// InPlaceDeduplication 原地去重要判断切片是否是有序的   如果是有序的那么复杂度就是n   如果不是有序的  先排序再去重nlogn+n  或者 n²
 // InPlaceDeduplication 原地去重
 func InPlaceDeduplication(num []int) []int {
+	if len(num) <= 1 {
+		return num
+	}
+	sort.Ints(num)
 
-	return nil
+	clear := 0
+	for i := 1; i < len(num); i++ {
+		if num[clear] != num[i] {
+			clear++
+			num[clear] = num[i]
+		}
+	}
+	return num[:clear+1]
+}
+
+func main() {
+	num := []int{1, 2, 1, 3, 5, 5, 7, 7, 8, 10}
+	inplace := InPlaceDeduplication(num)
+	fmt.Println(inplace)
 }
